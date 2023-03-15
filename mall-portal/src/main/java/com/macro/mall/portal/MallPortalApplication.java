1f56453cb4fc6585bf4891d01791d526bbbf8508
jecktion <wanggs_wkyx@163.com>
2023-03-15 09:16:55 +0800

    first commit

---
+++ mall-portal/src/main/java/com/macro/mall/portal/MallPortalApplication.java
@@ -0,0 +1,15 @@
+package com.macro.mall.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MallPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallPortalApplication.class, args);
+        System.out.println("http://localhost:8888");
+    }
+
+}
